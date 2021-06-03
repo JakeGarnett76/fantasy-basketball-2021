f322cd5e9a43d9555d9d44be6f34fd22de5649bd
Jake Garnett <jakegarnett6171@gmail.com>
2021-06-03 00:23:53 -0500

    Changes to Player Class

--- Backend/springboot/src/main/java/Teams/Players.java
+++ Backend/springboot/src/main/java/Teams/Players.java
@@ -1,4 +1,51 @@
 package Teams;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Players {
+
+
+    @Id
+    private int id;
+
+    private String name;
+    private String team;
+    private String manager;
+
+    Players() {
+
+    }
+
+    Players(String name, String team, String manager){
+        this.name = name;
+        this.team = team;
+        this.manager = manager;
+    }
+
+    public void setPlayerId(int id){
+        this.id = id;
+    }
+
+    public int getPlayerId(){
+        return id;
+    }
+
+    public void setTeam(String team){
+        this.team = team;
+    }
+
+    public String getPlayerTeam(){
+        return team;
+    }
+
+    public void setManager(String manager){
+        this.manager = manager;
+    }
+
+    public String getManager(){
+        return manager;
+    }
+
 }
