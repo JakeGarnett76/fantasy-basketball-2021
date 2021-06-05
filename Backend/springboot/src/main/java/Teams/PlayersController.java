d81ac2e228ddbc5d220229b8136ffbfa7acc915a
Jake Garnett <jakegarnett6171@gmail.com>
2021-06-04 23:44:33 -0500

    Added Player get and post requests

--- Backend/springboot/src/main/java/Teams/PlayersController.java
+++ Backend/springboot/src/main/java/Teams/PlayersController.java
@@ -1,4 +1,31 @@
 package Teams;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class PlayersController {
+
+    @Autowired
+    PlayersRepository playersRepository;
+
+    private String success = "{\"message\":\"success\"}";
+    private String failure = "{\"message\":\"failure\"}";
+
+    //get Player by Id
+    @GetMapping(path = "/players/{id}")
+    Players getPlayersById(@PathVariable int id){
+        return playersRepository.findById(id);
+    }
+
+    //post player
+    @PostMapping(path = "/players")
+    String postPlayers(@RequestBody Players player){
+        if(player == null){
+            return failure;
+        }
+        playersRepository.save(player);
+        return success;
+    }
+
 }
--- Backend/springboot/src/main/java/Teams/PlayersRepository.java
+++ Backend/springboot/src/main/java/Teams/PlayersRepository.java
@@ -1,4 +1,7 @@
 package Teams;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PlayersRepository {
+public interface PlayersRepository extends JpaRepository<Players, Long> {
+    Players findById(int id);
+    Players findByEntry(String Entry);
 }
