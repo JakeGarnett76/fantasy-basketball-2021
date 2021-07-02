61b7bee1bbe5a21d31c5901acd40dfea64fdbf0a
Jake Garnett <jakegarnett6171@gmail.com>
2021-07-01 23:55:32 -0500

    Began setting up Gameplay class

--- Backend/springboot/src/main/java/Summer/Game/Gameplay.java
+++ Backend/springboot/src/main/java/Summer/Game/Gameplay.java
@@ -1,8 +1,28 @@
 package Summer.Game;
 
+import org.springframework.data.annotation.Id;
+
 public class Gameplay {
+
+    @Id
+    private int id;
+
+    private String homeTeam;
+    private String awayTeam;
+    private int homeScore;
+    private int awayScore;
+
+
 
     Gameplay(){
 
+    }
+
+    Gameplay(String homeTeam, String awayTeam){
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+
+        this.homeScore = homeScore;
+        this.awayScore = awayScore;
     }
 }
