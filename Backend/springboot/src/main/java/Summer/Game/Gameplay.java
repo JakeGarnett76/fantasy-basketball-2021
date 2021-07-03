80006faed4f669e4c37aa680fe90ffb6d8ccb6b5
Jake Garnett <jakegarnett6171@gmail.com>
2021-07-02 23:39:00 -0500

    Added Getters and Setters to gameplay class

--- Backend/springboot/src/main/java/Summer/Game/Gameplay.java
+++ Backend/springboot/src/main/java/Summer/Game/Gameplay.java
@@ -25,4 +25,44 @@ public class Gameplay {
         this.homeScore = homeScore;
         this.awayScore = awayScore;
     }
+
+    public void setGameplayId(int id){
+        this.id = id;
+    }
+
+    public int getGameplayId(){
+        return id;
+    }
+
+    public void setHomeTeam(String homeTeam){
+        this.homeTeam = homeTeam;
+    }
+
+    public String getHomeTeam(){
+        return homeTeam;
+    }
+
+    public void setAwayTeam(String awayTeam){
+        this.awayTeam = awayTeam;
+    }
+
+    public String getAwayTeam(){
+        return awayTeam;
+    }
+
+    public void setHomeScore(int homeScore){
+        this.homeScore = homeScore;
+    }
+
+    public int getHomeScore(){
+        return homeScore;
+    }
+
+    public void setAwayScore(int awayScore){
+        this.awayScore = awayScore;
+    }
+
+    public int getAwayScore(){
+        return awayScore;
+    }
 }
