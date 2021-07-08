b44c7199b3a5c51b49abd565fd02d1cbd35bbbcf
Jake Garnett <jakegarnett6171@gmail.com>
2021-07-07 23:14:56 -0500

    Commit

--- Backend/springboot/src/main/java/Summer/Game/GameplayController.java
+++ Backend/springboot/src/main/java/Summer/Game/GameplayController.java
@@ -1,4 +1,26 @@
 package Summer.Game;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class GameplayController {
+
+    @Autowired
+    GameplayRepository gameplayRepository;
+
+    private String success = "{\"message\":\"success\"}";
+    private String failure = "{\"message\":\"failure\"}";
+
+    //get game score
+    @GetMapping(path = "/score")
+    List<String> getPlayers(){
+
+    }
+
+
 }
