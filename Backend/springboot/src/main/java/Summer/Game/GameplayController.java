af14519018837958faa531aa5602ce15f8aa7e0b
Jake Garnett <jakegarnett6171@gmail.com>
2021-07-11 23:37:00 -0500

    Work on controller

--- Backend/springboot/src/main/java/Summer/Game/GameplayController.java
+++ Backend/springboot/src/main/java/Summer/Game/GameplayController.java
@@ -1,8 +1,10 @@
 package Summer.Game;
 
 
+import Summer.Teams.Players;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,10 +18,16 @@ public class GameplayController {
     private String success = "{\"message\":\"success\"}";
     private String failure = "{\"message\":\"failure\"}";
 
-    //get game score
+    //get game scores
     @GetMapping(path = "/score")
-    List<String> getPlayers(){
+    List<Gameplay> getScores(){
+            return gameplayRepository.findAll();
+    }
 
+    //get game score by id
+    @GetMapping(path = "/score/{id}")
+    Gameplay getScore(@PathVariable int id){
+        return gameplayRepository.findById(id);
     }
 
 
--- Backend/springboot/src/main/java/Summer/Game/GameplayRepository.java
+++ Backend/springboot/src/main/java/Summer/Game/GameplayRepository.java
@@ -1,4 +1,11 @@
 package Summer.Game;
 
-public interface GameplayRepository {
+import Summer.Teams.Players;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameplayRepository extends JpaRepository<Gameplay, Long> {
+
+    Players findById(int id);
+//    Players findByEntry(String Entry);
+    Players findByManager(String manager);
 }
