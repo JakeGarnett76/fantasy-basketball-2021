b8eed912ab3f01b0aaf81b1c5592d1f9bc9d5da4
Jake Garnett <jakegarnett6171@gmail.com>
2021-07-20 20:51:19 -0500

    Bug fix. everything working

--- Backend/springboot/src/main/java/Summer/Game/GameplayRepository.java
+++ Backend/springboot/src/main/java/Summer/Game/GameplayRepository.java
@@ -7,5 +7,5 @@ public interface GameplayRepository extends JpaRepository<Gameplay, Long> {
 
     Gameplay findById(int id);
 //    Gameplay findByEntry(String Entry);
-    Gameplay findByManager(String manager);
+//    Gameplay findByManager(String manager);
 }
