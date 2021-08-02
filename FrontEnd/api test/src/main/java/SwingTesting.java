d50a340a91675f06581d4110c9d7f6ffff55aa27
Jake Garnett <jakegarnett6171@gmail.com>
2021-08-01 23:53:36 -0500

    Began work on frontend GUI

---
+++ FrontEnd/api test/src/main/java/SwingTesting.java
@@ -0,0 +1,28 @@
+
+    import javax.swing.*;
+    public class SwingTesting {
+        public static void main(String[] args) {
+            JFrame f=new JFrame();//creating instance of JFrame
+
+            JButton b=new JButton("Lineup");//creating instance of JButton
+            b.setBounds(130,100,100, 40);//x axis, y axis, width, height
+
+            JButton b2 = new JButton("Match up");
+            b2.setBounds(130, 150, 100, 40);
+
+            JButton b3 = new JButton("Exit");
+            b3.setBounds(130, 250,100,40);
+
+            JButton b4 = new JButton("Draft");
+            b4.setBounds(130, 200,100,40);
+
+            f.add(b);//adding button in JFrame
+            f.add(b2);
+            f.add(b3);
+            f.add(b4);
+
+            f.setSize(400,500);//400 width and 500 height
+            f.setLayout(null);//using no layout managers
+            f.setVisible(true);//making the frame visible
+        }
+    }
