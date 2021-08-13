55af4e1389da78c18e168b7dc8ecd377a28552a6
Jake Garnett <jakegarnett6171@gmail.com>
2021-08-12 23:55:41 -0500

    Added comments

--- FrontEnd/api test/src/main/java/SwingTesting.java
+++ FrontEnd/api test/src/main/java/SwingTesting.java
@@ -4,15 +4,19 @@
         public static void main(String[] args) {
             JFrame f=new JFrame();//creating instance of JFrame
 
+            //Adds Lineup Button
             JButton b=new JButton("Lineup");//creating instance of JButton
             b.setBounds(130,100,100, 40);//x axis, y axis, width, height
 
+            //Adds Match UP Button
             JButton b2 = new JButton("Match up");
             b2.setBounds(130, 150, 100, 40);
 
+            //Adds Exit Button
             JButton b3 = new JButton("Exit");
             b3.setBounds(130, 250,100,40);
 
+            //Adds Draft Button
             JButton b4 = new JButton("Draft");
             b4.setBounds(130, 200,100,40);
 
